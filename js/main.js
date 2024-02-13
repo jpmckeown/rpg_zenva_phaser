913e420a26161c3b50fe301c9510f17d386d9c58
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2024-02-13 10:33:04 +0000

    create game and scene

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const config = {
+   type: Phaser.AUTO,
+   width: 800,
+   height: 600,
+   scene: {
+      preload: preload,
+      create: create
+   }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+
+}
+function create() {
+
+}
