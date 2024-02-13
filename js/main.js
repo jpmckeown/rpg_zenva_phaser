20ac7bbaf87e46b2c7ce392a78271b28463673b2
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2024-02-13 14:44:52 +0000

    add keys moving compass and diagonals if two pressed

--- js/main.js
+++ js/main.js
@@ -4,7 +4,17 @@ const config = {
    height: 600,
    scene: {
       preload: preload,
-      create: create
+      create: create,
+      update: update
+   },
+   physics: {
+      default: 'arcade',
+      arcade: {
+         debug: true,
+         gravity: {
+            y: 0
+         }
+      }
    }
 };
 
@@ -13,15 +23,38 @@ const game = new Phaser.Game(config);
 function preload() {
    this.load.image('button1', 'assets/images/ui/blue_button01.png');
    this.load.image('button2', 'assets/images/ui/blue_button02.png');
-   this.load.image('items', 'assets/images/items.png');
+   this.load.spritesheet('items', 'assets/images/items.png', { frameWidth: 32, frameHeight: 32 });
+   this.load.spritesheet('characters', 'assets/images/characters.png', { frameWidth: 32, frameHeight: 32 });
 }
 
 function create() {
    let btn1 = this.add.image(100, 100, 'button1');
    btn1.setOrigin(0, 0);
-   let items = this.add.image(100, 300, 'items');
-   btn1.setOrigin(0, 0);
-   let btn2 = this.add.sprite(300, 100, 'button2');
-   btn2.setOrigin(0, 0);
+   let items = this.add.image(100, 300, 'items', 1);
+   let characters = this.add.image(100, 300, 'characters', 0);
+   this.player = this.physics.add.image(32, 32, 'characters', 0);
 
+   this.physics.add.image(400, 100, 'button2');
+   this.cursors = this.input.keyboard.createCursorKeys();
+}
+
+function update() {
+   if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-30);
+   }
+   else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(30);
+   }
+   else {
+      this.player.setVelocityX(0);
+   }
+   if (this.cursors.up.isDown) {
+      this.player.setVelocityY(-30);
+   }
+   else if (this.cursors.down.isDown) {
+      this.player.setVelocityY(30);
+   }
+   else {
+      this.player.setVelocityY(0);
+   }
 }
