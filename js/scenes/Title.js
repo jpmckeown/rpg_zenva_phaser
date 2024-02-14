02f89933d8be1d133908c13ceb1fa197e5d899fa
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2024-02-14 16:31:11 +0000

    align.in.center buttonText with button

--- js/scenes/Title.js
+++ js/scenes/Title.js
@@ -2,17 +2,16 @@ class Title extends Phaser.Scene {
    constructor() {
       super('Title');
    }
-   preload() {
 
-   }
    create() {
       this.titleText = this.add.text(this.scale.width / 2, this.scale.height / 2, 'Zenva RPG', { fontSize: '64px', fill: '#fff' })
          .setOrigin(0.5);
 
-      this.buttonText = this.add.text(0, 0, 'Start', { fontSize: '28px', fill: '#fff' });
-
       this.btn1 = this.physics.add.image(this.scale.width / 2, this.scale.height * 0.65, 'button1')
          .setInteractive();
+
+      this.buttonText = this.add.text(0, 0, 'Start', { fontSize: '28px', fill: '#000' });
+      Phaser.Display.Align.In.Center(this.buttonText, this.btn1, 0, -2);
 
       this.btn1.on('pointerdown', () => {
          console.log('pointer down');
