5bdb3a37fb3f9d77f600666b42356ee64bf2b1db
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2024-02-15 13:35:21 +0000

    add container UIbutton for image & text

---
+++ js/classes/UIbutton.js
@@ -0,0 +1,36 @@
+class UIbutton extends Phaser.GameObjects.Container {
+   constructor(scene, x, y, key, hoverKey, text, targetCallback) {
+      super(scene, x, y);
+      this.scene = scene;
+      Object.assign(this, { x, y, key, hoverKey, text, targetCallback });
+      this.createButton();
+      scene.add.existing(this);
+   }
+
+   createButton() {
+      this.btn1 = this.scene.add.image(0, 0, 'button1')
+         .setScale(1.5)
+         .setInteractive();
+
+      this.buttonText = this.scene.add.text(0, 0, this.text, { fontSize: '28px', fill: '#000' });
+
+      Phaser.Display.Align.In.Center(this.buttonText, this.btn1, 0, -2);
+
+      this.add(this.btn1);
+      this.add(this.buttonText);
+
+      this.btn1.on('pointerdown', () => {
+         console.log('pointer down');
+         this.targetCallback();
+         // this.scene.start('Game');
+      });
+      this.btn1.on('pointerover', () => {
+         console.log('pointer over');
+         this.btn1.setTexture(this.hoverKey);
+      });
+      this.btn1.on('pointerout', () => {
+         console.log('pointer out');
+         this.btn1.setTexture(this.key);
+      });
+   }
+}
--- js/scenes/Title.js
+++ js/scenes/Title.js
@@ -4,26 +4,12 @@ class Title extends Phaser.Scene {
    }
 
    create() {
-      this.titleText = this.add.text(this.scale.width / 2, this.scale.height / 2, 'Zenva RPG', { fontSize: '64px', fill: '#fff' })
-         .setOrigin(0.5);
+      this.titleText = this.add.text(this.scale.width / 2, this.scale.height / 2, 'Zenva RPG', { fontSize: '64px', fill: '#fff' }).setOrigin(0.5);
 
-      this.btn1 = this.physics.add.image(this.scale.width / 2, this.scale.height * 0.65, 'button1')
-         .setInteractive();
+      this.startGameButton = new UIbutton(this, this.scale.width / 2, this.scale.height * 0.65, 'button1', 'button2', 'Start', this.startScene.bind(this, 'Game'));
+   }
 
-      this.buttonText = this.add.text(0, 0, 'Start', { fontSize: '28px', fill: '#000' });
-      Phaser.Display.Align.In.Center(this.buttonText, this.btn1, 0, -2);
-
-      this.btn1.on('pointerdown', () => {
-         console.log('pointer down');
-         this.scene.start('Game');
-      });
-      this.btn1.on('pointerover', () => {
-         console.log('pointer over');
-         this.btn1.setTexture('button2');
-      });
-      this.btn1.on('pointerout', () => {
-         console.log('pointer out');
-         this.btn1.setTexture('button1');
-      });
+   startScene(targetScene) {
+      this.scene.start(targetScene);
    }
 }
